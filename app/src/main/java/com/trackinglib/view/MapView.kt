7c45dd04e27719a236569385f92571cd92e0e47a
alexaccandr <alexaccandr@gmail.com>
2019-01-04 17:42:06 +0300

    fix maps crash

--- app/src/main/java/com/trackinglib/view/MapView.kt
+++ app/src/main/java/com/trackinglib/view/MapView.kt
@@ -1,11 +1,15 @@
 package com.trackinglib.view
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.trackinglibrary.model.Track
 
 interface MapView : MvpView {
 
+    @StateStrategyType(value = SkipStrategy::class)
     fun onTrackLoaded(track: Track)
+
     fun updateStartDate(date: String)
     fun updateStartLocation(address: String)
     fun updateFinishLocation(address: String)
