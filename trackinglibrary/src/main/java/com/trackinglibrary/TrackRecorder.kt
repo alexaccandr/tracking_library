328c14d5995a5fc8db03c0ce90d5a21961dfacb3
alexaccandr <alexaccandr@gmail.com>
2019-01-24 03:46:28 +0300

    null checks

--- trackinglibrary/src/main/java/com/trackinglibrary/TrackRecorder.kt
+++ trackinglibrary/src/main/java/com/trackinglibrary/TrackRecorder.kt
@@ -11,11 +11,11 @@ import com.trackinglibrary.model.*
 import com.trackinglibrary.services.TrackingService
 import com.trackinglibrary.utils.ContextUtils
 import com.trackinglibrary.utils.DatabaseUtils
-import com.trackinglibrary.utils.LocationUtils
 import com.trackinglibrary.utils.RxBus
 import io.reactivex.Scheduler
 import io.reactivex.disposables.Disposable
 import io.realm.Realm
+import org.jetbrains.annotations.NotNull
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -30,7 +30,7 @@ object TrackRecorder {
 
     @Synchronized
     @JvmStatic
-    fun initialize(context: Application) {
+    fun initialize(@NotNull context: Application) {
         if (initialized) {
             Log.i("info", "Tracker already initialized")
             // already initialized
@@ -45,9 +45,6 @@ object TrackRecorder {
         if (shouldContinueRecording()) {
             continueRecording()
         }
-
-        // test suites
-        applyTestSuits()
     }
 
     @Synchronized
@@ -93,7 +90,7 @@ object TrackRecorder {
     }
 
     @JvmStatic
-    fun setFrequency(frequency: Long) {
+    fun setFrequency(@NotNull frequency: Long) {
         checkInitialized()
 
         if (frequency in TimeUnit.MINUTES.toMillis(1)..TimeUnit.MINUTES.toMillis(60)) {
@@ -118,7 +115,7 @@ object TrackRecorder {
     }
 
     @JvmStatic
-    fun getTrack(id: String): Track {
+    fun getTrack(@NotNull id: String): Track {
         val realm = Realm.getDefaultInstance()
         realm.use {
             val dao = TrackRecordDao(realm)
@@ -129,7 +126,8 @@ object TrackRecorder {
 
     @JvmStatic
     fun registerAverageSpeedChangeListener(
-        scheduler: Scheduler, listener: (TrackAverageSpeed) -> Unit
+        @NotNull scheduler: Scheduler,
+        @NotNull listener: (TrackAverageSpeed) -> Unit
     ): Disposable {
         return RxBus.listen(TrackAverageSpeed::class.java).observeOn(scheduler).subscribe {
             listener(it)
@@ -138,7 +136,8 @@ object TrackRecorder {
 
     @JvmStatic
     fun registerTrackStatusChangeListener(
-        scheduler: Scheduler, listener: (TrackStatus) -> Unit
+        @NotNull scheduler: Scheduler,
+        @NotNull listener: (TrackStatus) -> Unit
     ): Disposable {
         return RxBus.listen(TrackStatus::class.java).observeOn(scheduler).subscribe {
             Log.d(tag, "Listener called TrackStatus(${it.started})")
@@ -148,8 +147,8 @@ object TrackRecorder {
 
     @JvmStatic
     fun registerTrackLocationChangeListener(
-        scheduler: Scheduler,
-        listener: (TrackPoint) -> Unit
+        @NotNull scheduler: Scheduler,
+        @NotNull listener: (TrackPoint) -> Unit
     ): Disposable {
         return RxBus.listen(TrackPoint::class.java).observeOn(scheduler).subscribe {
             listener(it)
@@ -204,67 +203,4 @@ object TrackRecorder {
     internal fun executeUpdateAverageSpeed(data: Pair<Long, Double>) {
         handlerQueue.putQueueUpdateAverageSpeed(data)
     }
-}
-
-private fun TrackRecorder.applyTestSuits() {
-
-    val trackCount = Realm.getDefaultInstance().use {
-        TrackRecordDao(it).hasTracks()
-    }
-
-    // already has records, skip suites
-    if (trackCount > 0) {
-        return
-    }
-
-    fun runTest(startTime: Long, latitude: Double, longitude: Double, stepPerMinute: Double) {
-        fun createLocation(lat: Double, lon: Double, time: Long): Location {
-            val location = Location("gps")
-            location.latitude = lat
-            location.longitude = lon
-            location.time = time
-            return location
-        }
-
-
-        val realm: Realm = Realm.getDefaultInstance()
-        val dao = TrackRecordDao(realm)
-        realm.executeTransaction {
-            val track = dao.createTrack(startTime)
-
-            var loc1: Location? = null
-            var loc2: Location? = null
-            for (i in 0..100) {
-                loc1 = if (loc1 == null) {
-                    createLocation(latitude, longitude, startTime)
-                } else {
-                    loc2
-                }
-                loc2 = createLocation(
-                    loc1!!.latitude + stepPerMinute,
-                    loc1.longitude,
-                    loc1.time + TimeUnit.MINUTES.toMillis(1)
-                )
-
-                TrackRecordDao(it).saveLocation(
-                    track.id,
-                    loc2.latitude,
-                    loc2.longitude,
-                    loc2.time
-                )
-                track.totalTime = track.totalTime + (loc2.time - loc1.time)
-                track.totalDistance = track.totalDistance + LocationUtils.calcDistance(loc1, loc2)
-            }
-            dao.stopTrack(track.id, loc2!!.time)
-        }
-    }
-
-    val startTime = Calendar.getInstance().timeInMillis - TimeUnit.DAYS.toMillis(3)
-    runTest(startTime, 45.045447, 38.978492, 0.01)
-
-    val startTime2 = Calendar.getInstance().timeInMillis - TimeUnit.DAYS.toMillis(2)
-    runTest(startTime2, 55.759203, 37.619121, 0.015)
-
-    val startTime3 = Calendar.getInstance().timeInMillis - TimeUnit.DAYS.toMillis(1)
-    runTest(startTime3, 59.937245, 30.331714, 0.012)
 }
